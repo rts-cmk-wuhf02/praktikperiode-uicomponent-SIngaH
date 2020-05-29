48a6dc36b1c7cd35002ebb01a84cfc38d3a8467f
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-05-29 09:21:36 +0200

    gatsby og contentful

--- uicomponents/src/components/Surfing.js
+++ uicomponents/src/components/Surfing.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react"
 import Img1 from "../imgs/Rectangle 2.png"
 import "../css/surfing.css"
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
 
 class Surfing extends Component{
     constructor(){
@@ -11,6 +13,7 @@ class Surfing extends Component{
     }
 
     render(){
+        const title = get(this, 'props.data.ContentfulBlogPost.title')
         return(
            <div id="Surfing">
                <img src={Img1} alt="Surfing"/>
@@ -18,9 +21,30 @@ class Surfing extends Component{
                    <h4>TRAVEL</h4>
                    <h1>Surfing in Maldives</h1>
                    <p>It’s windy. The cool breeze of the ocean. It gives, a sense of beauty, in motion. All is flowing, rushing and tide-And I sit in wonder, dreaming beside.</p>
+                   <p>{title}</p>
                </div>
            </div>
         )
     }
 }
 export default Surfing
+
+export const pageQuery = graphql`
+    query MyQuery {
+        contentfulBlogPost(slug: {eq: "Surfing"}) {
+          slug
+          title
+          heroImage {
+            title
+            file {
+              fileName
+              url
+            }
+          }
+          description {
+            id
+            description
+          }
+        }
+    }
+  `
