673ebd8245c5900d61c7552da5d0ccbb0d03853e
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-05-29 11:25:26 +0200

    gatsby start

--- uicomponents/src/components/Surfing.js
+++ uicomponents/src/components/Surfing.js
@@ -1,50 +1,17 @@
-import React, {Component} from "react"
-import Img1 from "../imgs/Rectangle 2.png"
-import "../css/surfing.css"
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
+import React from "react"
+// import "../css/surfing.css"
+import Img from 'gatsby-image'
 
-class Surfing extends Component{
-    constructor(){
-        super()
-        this.state = {
-            articles : []
-        }
-    }
-
-    render(){
-        const title = get(this, 'props.data.ContentfulBlogPost.title')
-        return(
-           <div id="Surfing">
-               <img src={Img1} alt="Surfing"/>
-               <div className="surfing-text">
-                   <h4>TRAVEL</h4>
-                   <h1>Surfing in Maldives</h1>
-                   <p>It’s windy. The cool breeze of the ocean. It gives, a sense of beauty, in motion. All is flowing, rushing and tide-And I sit in wonder, dreaming beside.</p>
-                   <p>{title}</p>
-               </div>
-           </div>
-        )
-    }
+function Surfing({ title, label, content, images }) {
+  return(
+    <div id="Surfing">
+        { <Img fluid={images[0].fluid} alt="Surfing"/> }
+        <div className="surfing-text">
+            <h4>{ label }</h4>
+            <h1>{title}</h1>
+            <p>{content.content}</p>
+        </div>
+    </div>
+  )
 }
 export default Surfing
-
-export const pageQuery = graphql`
-    query MyQuery {
-        contentfulBlogPost(slug: {eq: "Surfing"}) {
-          slug
-          title
-          heroImage {
-            title
-            file {
-              fileName
-              url
-            }
-          }
-          description {
-            id
-            description
-          }
-        }
-    }
-  `
--- uicomponents/src/pages/index.js
+++ uicomponents/src/pages/index.js
@@ -1,86 +1,31 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
-import Hero from '../components/hero'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import Surfing from "../components/Surfing"
 
-class RootIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
-
-    return (
-      <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-        </div>
-      </Layout>
-    )
-  }
+function RootIndex({ data }) {
+  let travelData = data.allContentfulTravel.nodes[0];
+  console.log(travelData)
+  return (
+    <Surfing { ...travelData } />
+  )
 }
 
 export default RootIndex
 
-export const pageQuery = graphql`
-  query HomeQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-          description {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
+export const query = graphql`
+query travelQuery {
+  allContentfulTravel {
+    nodes {
+      title
+      label
+      content {
+        content
       }
-    }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      edges {
-        node {
-          name
-          shortBio {
-            shortBio
-          }
-          title
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
         }
       }
     }
   }
-`
+}
+  `
